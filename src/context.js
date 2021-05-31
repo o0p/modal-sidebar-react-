9c5be94c0e5ddfe2d22381ef110c41b4652c825e
Ruslan <rus@gmx.com>
2021-05-31 15:59:48 +0100

    Add open/close sidebar functions

--- src/context.js
+++ src/context.js
@@ -5,6 +5,14 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
 };
 //custo hook

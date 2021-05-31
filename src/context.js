0147d3cb10d7fdcadcb8aca0b0e7f3774ebd3476
Ruslan <rus@gmx.com>
2021-05-31 15:58:33 +0100

    Add sidebar and modal states

--- src/context.js
+++ src/context.js
@@ -3,7 +3,10 @@ import React, { useState, useContext } from 'react';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
 };
+//custo hook
 
 export { AppContext, AppProvider };
